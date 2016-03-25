4f177010a251ac37fcde580dba2bd122a8a06aa5
PopovMihail <popov-mm@ya.ru>
2016-03-25 20:20:31 +0700

    Added files via upload

--- strings.c
+++ strings.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 void sspn(char *s);
 int slen(char *s);
 int stok(char *str, char delim, char *ptr[]);	
@@ -8,31 +9,31 @@ int schr(char *str, char delim);
 int sequal(char *s, char *s2);
 void scopy(char *s, char *s2);
 void suntok(char *str, char delim, char *ptr[], int cnt);
-
+void shift(char *ptr, char *ptr2, char *str, int lengs, int idx, int delta, int s, int j);
 
 int main()
 {
 	int i,j,k,t,s,delta, d_str, j_sdvig, index;
-	char dir1[15];// = "C:\\Windows";
-	char dir2[15];// = "E:\\WindowsXP";
+	char dir1[15] = "win";
+	char dir2[15] = "winXP";
 	char del;
 	char raz = '\\';
-	char str[260];// = "C:\\Windows\\system32+C:\\User\\test+C:\\user\\explorer.exe+C:\\Windows\\Distrib";
+	char str[260] = "C:\\win\\explorer.exe+C:\\Wpn\\sys\\tem32+C:\\win\\Distrib";
 	char *ptr[10];
-	char *ptr_dir1[3];
-	char *ptr_raz[6];
+	char *ptr_dir1[10];
+	char *ptr_raz[10];
 	printf("delim: ");
 	scanf("%c",&del);
 	char *suf = str;
 //C:\\Windows\\system32+C:\\User\\test+C:\\user\\explorer.exe+C:\\Windows\\Distrib
-	printf("paths: ");
+/*	printf("paths: ");
 	scanf("%s",&str);
 		
 	printf("dir1: ");
 	scanf("%s",&dir1);
 	
 	printf("dir2: ");
-	scanf("%s",&dir2);
+	scanf("%s",&dir2);*/
 	
 	delta = slen(dir1) - slen(dir2);
 	d_str = slen(str);
@@ -40,10 +41,10 @@ int main()
 	i = stok(str, del, ptr);
 	k = stok(dir1, raz, ptr_dir1);
 	//t = stok(ptr[0], raz, ptr_raz);
-	
+	//printf("k = %d\n", k);
 	
 	int f = 0;
-	s = 0;
+	
 	
 	for(j = 0;  j < i; j++) {
 		sspn(ptr[j]);
@@ -51,16 +52,11 @@ int main()
 	
 	for (j = 0;  j < i; j++) {
 		t = stok(ptr[j], raz, ptr_raz);
-		
+		if(k > 1){
 		for (s = 0;  s < k; s++) {
-		
 			f += sequal(ptr_dir1[s], ptr_raz[s]);
-			//printf("F = %d\n", f);
-			if (f == 2) {
-			
+			if (f == k) {
 				index = ptr_raz[s+1] - suf;	
-				//printf("index= %d\n ", index);
-						
 				if (delta < 0) {
 					for (j_sdvig = d_str - delta;  j_sdvig > index + delta; j_sdvig--) {
 						str[j_sdvig - delta] = str[j_sdvig];
@@ -83,16 +79,54 @@ int main()
 					}
 					ptr_raz[s + 1] = ptr_raz[s + 1] - delta;		
 				}
+							//shift(*ptr, *ptr_raz, str, d_str, index, delta, s, j);
 				suntok(ptr[j], raz, ptr_raz, t);
-						
-				scopy(dir2, ptr[j]);
-				
+				scopy(dir2, ptr[j]);	
+				f = 0;
 			}
-
+			
 			suntok(ptr[j], raz, ptr_raz, t);
 		}
 		
-		f = 0;
+		}
+		
+		for (s = 0;  s < t; s++) {
+			f += sequal(dir1, ptr_raz[s]);
+			//printf("f = %d\n", f);
+			if(f == 1){
+				//printf("s = %d\n", s);
+				index = ptr_raz[s+1] - suf;	
+				///printf("index= %d\n ", index);
+						
+				if (delta < 0) {
+					for (j_sdvig = d_str - delta;  j_sdvig > index + delta; j_sdvig--) {
+						str[j_sdvig - delta] = str[j_sdvig];
+					}
+					
+					for (j_sdvig = j;  j_sdvig < i; j_sdvig++) {
+						ptr[j_sdvig + 1] = ptr[j_sdvig + 1] - delta;
+					}
+					
+					ptr_raz[s + 1] = ptr_raz[s + 1] - delta;		
+				}
+				
+				if (delta > 0) {
+					for (j_sdvig = index;  j_sdvig < d_str; j_sdvig++) {
+						str[j_sdvig - delta] = str[j_sdvig];
+					}
+					str[d_str-delta] = '\0';
+					for (j_sdvig = j;  j_sdvig < i; j_sdvig++) {
+						ptr[j_sdvig + 1] = ptr[j_sdvig + 1] - delta;
+					}
+					ptr_raz[s + 1] = ptr_raz[s + 1] - delta;
+				}
+				scopy(dir2, ptr_raz[s]);
+			f = 0;			
+			}
+			suntok(ptr[j], raz, ptr_raz, t);
+		}
+		
+		//f = 0;
 	}
 	
 	suntok(str, del, ptr, i);
@@ -100,9 +134,35 @@ int main()
 	
 	return 0;
 }
+/*
+void shift(char *ptr, char *ptr2, char *str, int lengs, int idx, int delta, int s, int j)
+{
+	int i;
+	if (delta < 0) {
+		for (i = lengs - delta; i > idx + delta; i--) {
+			str[i - delta] = str[i];
+		}
+					
+		for (i = j;  i < i; i++) {
+			ptr[i + 1] = ptr[i + 1] - delta;
+		}					
+		ptr2[s + 1] = ptr2[s + 1] - delta;		
+	}
+				
+	if (delta > 0) {
+		for (i = idx;  i < lengs; i++) {
+			str[i - delta] = str[i];
+		}
+		str[lengs-delta] = '\0';
+		for (i = j;  i < i; i++) {
+			ptr[i + 1] = ptr[i + 1] - delta;
+		}
+		ptr2[s + 1] = ptr2[s + 1] - delta;		
+	}
+}
+*/
 
-
-/*РєРѕРїРёСЂРѕРІР°РЅРёРµ СЃС‚СЂРѕРє*/
+ //копирование строк
 void scopy(char *s, char *s2)
 {
 	int i;
@@ -110,14 +170,14 @@ void scopy(char *s, char *s2)
 		s2[i] = s[i];
 	
 }
-/*РѕРїСЂРµРґРµР»РµРЅРёРµ РґР»РёРЅРЅС‹ СЃС‚СЂРѕРєРё*/
+ //определение длинны строки
 int slen(char *s)
 {
     int i;
     for (i = 0; s[i] != '\0'; i++);
     return i;
 }
-/*СЂР°Р·Р±РёРµРЅРёРµ СЃС‚СЂРѕРєРё РЅР° РїРѕРґСЃС‚СЂРѕРєРё*/
+///*разбиение строки на подстроки
 int stok(char *str, char delim, char *ptr[])
 {
 	char *suf = str;
@@ -133,7 +193,7 @@ int stok(char *str, char delim, char *ptr[])
 	
 	return j;
 }
-/*СЃРѕР±РёСЂР°РЅРёРµ РїРѕРґСЃС‚СЂРѕРє РІ РёСЃС…РѕРґРЅСѓСЋ СЃС‚СЂРѕРєСѓ*/
+//*собирание подстрок в исходную строку
 void suntok(char *str, char delim, char *ptr[], int cnt)
 {
 	int i;
@@ -141,7 +201,7 @@ void suntok(char *str, char delim, char *ptr[], int cnt)
 		*(ptr[i] - 1) = delim;
 	}
 }
-/*РїРѕРёСЃРє СЃРёРјРІРѕР»Р° РІ СЃС‚СЂРѕРєРµ*/
+//*поиск символа в строке
 int schr(char *str, char delim)
 {
 	int i, idx = -1;
@@ -150,7 +210,7 @@ int schr(char *str, char delim)
 			idx = i;
 return idx;
 }
-/*СЃСЂР°РІРЅРµРЅРёРµ РґРІСѓС… СЃС‚СЂРѕРє*/ 
+//*сравнение двух строк
 int sequal(char *s, char *s2)
 {
 	int i, out = 1;
@@ -165,14 +225,14 @@ int sequal(char *s, char *s2)
 	
 	return out;
 }
-/*РїСЂРѕРІРµСЂРєР° СЃС‚СЂРѕРєРё РЅР° РЅРµ РґРѕРїСѓСЃС‚РёРјС‹Рµ СЃРёРјРІРѕР»С‹*/
+//*проверка строки на не допустимые символы
 
 void sspn(char *s)
 {
 	int i;
 	
 	for(i = 0;  s[i] != '\0'; i++){
-		if (s[i] ==  '*' ){
+		if ((s[i] == '*') || (s[i] == '?') || (s[i] == '"') || (s[i] == '>') || (s[i] == '<') || (s[i] == '|') ){
 			
 			fprintf(stderr, "invalid character number %d \n", i+1);
 			printf("%s\n",s);
@@ -180,4 +240,3 @@ void sspn(char *s)
 		}
 	}
 }
-
